2c89213963efcee5cfc9bb72823a043dc7686a69
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-05-21 12:52:48 +0100

    Return an array of winners

--- index.js
+++ index.js
@@ -57,13 +57,19 @@ console.log(getYears(fifaData));
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(finalsArr) {
 
-    /* code here */
-
+    const winners = finalsArr.map(obj => {
+        if (obj['Home Team Goals'] > obj['Away Team Goals']) {
+            return obj['Home Team Name'];
+        } else {
+            return obj['Away Team Name'];
+        }
+    });
+    return winners;
 };
 
-getWinners();
+console.log(getWinners(fifaData));
 
 /* Task 6: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 Parameters: 
@@ -87,40 +93,3 @@ function getAverageGoals(/* code here */) {
 
 getAverageGoals();
 
-/// STRETCH 🥅 //
-
-/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
-Hint: Investigate your data to find "team initials"!
-Hint: use `.reduce` */
-
-function getCountryWins(/* code here */) {
-
-    /* code here */
-
-};
-
-getCountryWins();
-
-
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getGoals();
-
-
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
-
-function badDefense(/* code here */) {
-
-    /* code here */
-
-};
-
-badDefense();
-
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
