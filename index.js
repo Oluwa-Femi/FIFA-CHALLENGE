4301feed5526f8b3c99688d4c74500ae077ccd38
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-05-21 10:57:54 +0100

    Get finals

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ console.log(fifaData[828]['Win conditions']);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(arr) {
+        const finalsArr = arr.filter(obj => obj['Stage'] === 'Final')
+        return finalsArr;
 };
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
